6f62b8e942ace29bdce8ba4916a55e18d76191b7
Nihar Singla <nihar.singla@mettl.com>
2021-01-21 16:13:26 +0530

    react tooltip project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import Tippy from '@tippyjs/react'
 import './App.css';
+import 'tippy.js/dist/tippy.css'
+import React, { forwardRef } from 'react'
+
+const ColoredToolTip = () => {
+  return <span style={{ color: 'red' }}>Red Tooltip</span>
+}
+
+const CustomChild = forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <div>First Line</div>
+      <div>Second Line</div>
+    </div>
+  )
+}
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy arrow={false} delay={1000} placement={'right'} content='Basic Tooltip'>
+          <button>Hover over me!!</button>
+        </Tippy>
+      </div>
+
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy content={<span style={{ color: 'orange' }}>Colored</span>}>
+          <button>Hover over me!!</button>
+        </Tippy>
+      </div>
+
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy content={<ColoredToolTip></ColoredToolTip>}>
+          <button>Hover over me!!</button>
+        </Tippy>
+      </div>
+
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy placement={'top-end'} content={<ColoredToolTip></ColoredToolTip>}>
+          <CustomChild></CustomChild>
+        </Tippy>
+      </div>
     </div>
   );
 }
